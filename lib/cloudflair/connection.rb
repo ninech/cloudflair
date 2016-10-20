8ab364e731c75efa2276c3bd9df495aaf07b79eb
Christian Mäder <cma@nine.ch>
2016-10-20 10:13:45 +0200

    Move connection to Cloudflair module

--- lib/cloudflair/connection.rb
+++ lib/cloudflair/connection.rb
@@ -1,25 +1,27 @@
-class Connection
-  def self.new
-    config = Cloudflair.config
+module Cloudflair
+  class Connection
+    def self.new
+      config = Cloudflair.config
 
-    Faraday.new(url: config.cloudflare.api_base_url, headers: headers) do |faraday|
-      faraday.request  :url_encoded
-      faraday.response :logger
-      faraday.response :json, content_type: /\bjson$/
+      Faraday.new(url: config.cloudflare.api_base_url, headers: headers) do |faraday|
+        faraday.request :url_encoded
+        faraday.response :logger
+        faraday.response :json, content_type: /\bjson$/
 
-      faraday.adapter config.faraday.adapter || Faraday.default_adapter
+        faraday.adapter config.faraday.adapter || Faraday.default_adapter
+      end
     end
-  end
 
-  def self.headers
-    headers = {}
-    cloudflare_auth_config = Cloudflair.config.cloudflare.auth
-    if cloudflare_auth_config.user_service_key.nil?
-      headers['X-Auth-Key'] = cloudflare_auth_config.key
-      headers['X-Auth-Email'] = cloudflare_auth_config.email
-    else
-      headers['X-Auth-User-Service-Key'] = cloudflare_auth_config.user_service_key
+    def self.headers
+      headers = {}
+      cloudflare_auth_config = Cloudflair.config.cloudflare.auth
+      if cloudflare_auth_config.user_service_key.nil?
+        headers['X-Auth-Key'] = cloudflare_auth_config.key
+        headers['X-Auth-Email'] = cloudflare_auth_config.email
+      else
+        headers['X-Auth-User-Service-Key'] = cloudflare_auth_config.user_service_key
+      end
+      headers
     end
-    headers
   end
 end
