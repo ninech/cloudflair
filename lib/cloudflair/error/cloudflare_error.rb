b169cd65b1322687882067b55e5f0f955ac19ded
Christian Mäder <cma@nine.ch>
2016-10-27 12:22:07 +0200

    Improves the exception message of CloudFlare Error
    ...when no error message / code was sent by CloudFlare.

--- lib/cloudflair/error/cloudflare_error.rb
+++ lib/cloudflair/error/cloudflare_error.rb
@@ -9,6 +9,8 @@ module Cloudflair
     end
 
     def to_s
+      return '[ "An error happened, but no error message/code was given by CloudFlare." (Code: 0000) ]' if cloudflare_errors.empty?
+
       strings = cloudflare_errors.map { |cf_e| "\"#{cf_e[:message]}\" (Code: #{cf_e[:code]})" }
       "[ #{strings.join ', '} ]"
     end
