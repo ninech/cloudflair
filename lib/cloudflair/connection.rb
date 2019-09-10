51c1446ac97d2e960eccd50e131e29ecb96d9fdf
Martin Streicher <martin.streicher@gmail.com>
2019-09-10 09:58:00 -0400

    Address PR comments and use more guard-clause-like structure.

--- lib/cloudflair/connection.rb
+++ lib/cloudflair/connection.rb
@@ -12,22 +12,29 @@ module Cloudflair
       new_faraday_from config
     end
 
-    def self.headers
-      {}.tap do |request_headers|
-        cloudflare_auth_config = Cloudflair.config.cloudflare.auth
+    def self.headers # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      cloudflare_auth_config = Cloudflair.config.cloudflare.auth
 
-        if !(cloudflare_auth_config.key.nil? || cloudflare_auth_config.email.nil?)
-          request_headers['X-Auth-Key']   = cloudflare_auth_config.key
-          request_headers['X-Auth-Email'] = cloudflare_auth_config.email
-        elsif !cloudflare_auth_config.user_service_key.nil?
-          request_headers['Authorization'] = "Bearer #{cloudflare_auth_config.user_service_key}"
-        else
-          raise CloudflairError, 'Neither email & key nor user_service_key have been defined.'
-        end
+      if !cloudflare_auth_config.key.nil? && !cloudflare_auth_config.email.nil?
+        return({
+          'X-Auth-Key'   => cloudflare_auth_config.key,
+          'X-Auth-Email' => cloudflare_auth_config.email
+        })
       end
+
+      unless cloudflare_auth_config.user_service_key.nil?
+        return({
+          'Authorization' => "Bearer #{cloudflare_auth_config.user_service_key}"
+        })
+      end
+
+      raise(
+        CloudflairError,
+        'Neither email & key nor user_service_key have been defined.'
+      )
     end
 
-    private_class_method def self.new_faraday_from(config)
+    private_class_method def self.new_faraday_from(config) # rubocop:disable Metrics/AbcSize
       Faraday.new(url: config.cloudflare.api_base_url, headers: headers) do |faraday|
         faraday.request :json
         faraday.response config.faraday.logger if config.faraday.logger
